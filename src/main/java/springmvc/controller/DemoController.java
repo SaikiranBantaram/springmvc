fb33443ba1a3c98be45a33cc39713610eba9d68a
SaikiranBantaram <43469173+SaikiranBantaram@users.noreply.github.com>
2024-07-03 20:26:54 +0530

    added demo

--- src/main/java/springmvc/controller/DemoController.java
+++ src/main/java/springmvc/controller/DemoController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
-    public static void main(String[] args) {
-        System.out.println("demo controller");
-    }
+   public String test(){
+       String message ="hello";
+       return message;
+   }
 }
