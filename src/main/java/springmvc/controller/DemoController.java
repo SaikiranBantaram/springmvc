4a4600b7a4997f518b8db07809cd8b6c03f2c6b2
SaikiranBantaram <43469173+SaikiranBantaram@users.noreply.github.com>
2024-07-03 20:31:36 +0530

    added demo

--- src/main/java/springmvc/controller/DemoController.java
+++ src/main/java/springmvc/controller/DemoController.java
@@ -1,11 +1,15 @@
 package springmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springmvc.service.UserService;
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+    @Autowired
+    private UserService userService;
    public String test(){
        String message ="hello";
        return message;
