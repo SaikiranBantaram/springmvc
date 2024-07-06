3e88d4b499bce0eecbfd688e74a69e83f8c09ee5
SaikiranBantaram <43469173+SaikiranBantaram@users.noreply.github.com>
2024-07-06 15:40:41 +0530

    added demo
    added

---
+++ src/main/java/springmvc/service/DemoService.java
@@ -0,0 +1,11 @@
+package springmvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    public String demo(){
+        return "demo service";
+    }
+}
